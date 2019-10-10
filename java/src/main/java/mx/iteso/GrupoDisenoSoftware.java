a0e0d493781f796e13390e1b1dfca79b8373064d
JoseLuis <jltorrenterash@gmail.com>
2019-10-10 07:55:15 -0500

    Patron implementado

--- java/src/main/java/mx/iteso/GrupoDisenoSoftware.java
+++ java/src/main/java/mx/iteso/GrupoDisenoSoftware.java
@@ -1,5 +1,30 @@
 package mx.iteso;
 
-public class GrupoDisenoSoftware {
+public class GrupoDisenoSoftware implements Subject {
+    private final ArrayList observers;
+    private String message;
 
+    public GrupoDisenoSoftware(){
+        observers = new ArrayList();
+    }
+
+    public void registerObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer){
+        observers.remove(observers.indexOf(observer));
+    }
+
+    public void notifyObservers(){
+        for (Object observer1 : observers) {
+            Observer observer = (Observer) observer1;
+            observer.getMessage(this.message)
+        }
+    }
+
+    public void sendMessage(String msg){
+        this.message = msg;
+        notifyObservers();
+    }
 }
---
+++ java/src/main/java/mx/iteso/Observer.java
@@ -0,0 +1,12 @@
+package mx.iteso;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rvillalobos
+ * Date: 8/14/13
+ * Time: 2:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Observer {
+    void getMessage(String message);
+}
